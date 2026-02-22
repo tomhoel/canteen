709a54abb9b9d519af0732ecf4d101897c3cb995
Tom Hoel <tomhoel96@gmail.com>
2026-02-22 21:40:17 +0100

    Revert "feat: add animated ShaderGradient background for desktop"
    This reverts commit 70eaa5c228e1fe7d37a2ef6c1e3884819acef1e0.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ShaderBackground } from "@/components/ShaderBackground";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -49,7 +48,6 @@ export default function RootLayout({
         <meta httpEquiv="Expires" content="0" />
       </head>
       <body suppressHydrationWarning className={inter.className}>
-        <ShaderBackground />
         {children}
       </body>
     </html>
